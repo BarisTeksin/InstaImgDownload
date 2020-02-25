ebe5f4a7345d7754ce1ec80b05db79ca1c4f4715
Can Okay <canokay2014@gmail.com>
2020-02-26 02:39:36 +0300

    0.3.2 - Download type added.

--- run.py
+++ run.py
@@ -1,11 +1,30 @@
 from InstaImgDownload import login, DownloadProfilePhotos, DownloadTagPhotos
+import sys
 
 username = input('Instagram Username : ')
 passwd = input('Instagram Password : ')
-download_username = input('Download Instagram Username : ')
-download_post_count = int(input('Download Instagram User post count : '))
-
-
-login(username,passwd)
-
-DownloadProfilePhotos(download_username,download_post_count)
+while True:
+    download_username = input('Download Instagram Username : ')
+    print("""
+    ------------------------------------------------------
+    1) Photo
+    2) Tag Photo
+    ------------------------------------------------------""")
+    select = int(input('Select mode : '))
+    try:
+        if select == 1:
+            login(username,passwd)
+            DownloadProfilePhotos(download_username)
+        elif select == 2:
+            login(username,passwd)
+            DownloadTagPhotos(download_username)
+        else:
+            print('Error failled select.')
+    except ValueError:
+        print('Please write only number.')
+    control = input('Do you want to continue? (y/n) : ')
+    
+    if control == 'n' or control == 'N' or control == 'no' or control == 'NO':
+        break
+print('Quit now bot.')
+sys.exit()
